36e0f9f67c864d7f45315893cec8aaf52a3381f0
Luis McKinnon <mckano54@gmail.com>
2022-01-15 21:37:04 -0600

    Adding Movement Script to Player

---
+++ My First Game Jam/Assets/Mckano/Scripts/Player_Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        rb.velocity = new Vector3(horizontal * movementSpeed, rb.velocity.y, rb.velocity.z);
+    }
+}
