088d4b920322b3e5e6ae7eddcf59c182f2b75353
Daria Kolodzey <kolodaria@gmail.com>
2022-01-17 19:09:29 +0300

    Add comments to code

--- My First Game Jam/Assets/Oktosha/Scripts/OnionLayer.cs
+++ My First Game Jam/Assets/Oktosha/Scripts/OnionLayer.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class OnionLayer : MonoBehaviour
 {
+    // -- Movement controls --
+
+    // controls the amount of rotation that moves the object
     public float torque;
+
+    // controls the amount shift in the desired direction to get unstuck
+    // should be a small value, as the main control is done by torque
     public float directTransformShift;
-    public Rigidbody2D rb;
+
+    // -- End Movement controls --
+
+    private Rigidbody2D rb;
 
     void Start()
     {
@@ -15,8 +24,16 @@ public class OnionLayer : MonoBehaviour
 
     void FixedUpdate()
     {
+        // -- Movement --
+
         float input = Input.GetAxis("Horizontal");
+
+        // primary way of movement -> rotate itself to roll
         rb.AddTorque(-torque * input);
+
+        // small shift in the desired direction to get unstuck
         transform.position += Vector3.right * input * directTransformShift;
+
+        // -- End Movement --
     }
 }
